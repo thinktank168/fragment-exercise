526e9b4d159521ab9141459699d30160501fc0e2
thinktank168 <ky.tan168@gmail.com>
2017-07-09 12:56:38 +0800

    1st commit for  Fragment  Lifecycle

---
+++ app/src/main/java/itenabler/com/lesson3fragmentexercise/HomeFragment.java
@@ -0,0 +1,24 @@
+package itenabler.com.lesson3fragmentexercise;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kentan on 9/7/2017.
+ */
+public class HomeFragment  extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_homefragment, container, false);
+
+
+        return rootView;
+    }
+}
